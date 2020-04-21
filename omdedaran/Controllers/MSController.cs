9ad2364cce2bb288d2d66d5174c239e3dcd84efe
mohammad <mohammad@DESKTOP-NR7GHJV>
2020-04-21 12:28:42 +0430

    2

--- omdedaran/Controllers/MSController.cs
+++ omdedaran/Controllers/MSController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using omdedaran.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,74 @@ namespace omdedaran.Controllers
     public class MSController : Controller
     {
         // GET: MS
+
+        
+        ////////////////////////{ start : Index }////////////////////////1
         public ActionResult Index()
         {
             return View();
         }
+        /// /////////////////////{ end : Index }////////////////////////
+
+
+        ////////////////////////{ start : about }////////////////////////2
+      
+        public ActionResult about()
+        {
+            tbl_BLOG_TeamMembers tbt = new tbl_BLOG_TeamMembers();
+            
+
+            return View(tbt.Service());
+        }
+        /// /////////////////////{ end : about }////////////////////////
+
+
+        ////////////////////////{ start : contact }////////////////////////3
+        public ActionResult contact()
+        {
+            return View();
+        }
+        /// /////////////////////{ end : contact }////////////////////////
+
+
+
+        ////////////////////////{ start : login }////////////////////////4
+        public ActionResult login()
+        {
+            return View();
+        }
+        /// /////////////////////{ end : login }////////////////////////
+
+
+        ////////////////////////{ start : blog }////////////////////////5
+        public ActionResult blog()
+        {
+            return View();
+        }
+        /// /////////////////////{ end : blog }////////////////////////
+
+
+        ////////////////////////{ start : terms }////////////////////////6
+        public ActionResult terms()
+        {
+            return View();
+        }
+        /// /////////////////////{ end : terms }////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ omdedaran/Models/tbl_BLOG_TeamMembers.cs
@@ -0,0 +1,59 @@
+﻿using DataBaseConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace omdedaran.Models
+{
+    public class tbl_BLOG_TeamMembers
+    {
+      public string Id { get; set; }
+      public string Name { get; set; }
+      public string Job { get; set; }
+      public string Tozihat { get; set; }
+      public string github { get; set; }
+      public string Linkedin { get; set; }
+      public string Instagram { get; set; }
+      public string ImagePath { get; set; }
+      public string ImageValue { get; set; }
+    
+        //////////////////////////////////
+
+        public List<tbl_BLOG_TeamMembers> Service()
+        {
+            PDBC db = new PDBC("PandaMarketCMS", true);
+            db.Connect();
+
+            List<tbl_BLOG_TeamMembers> TeamMembers = new List<tbl_BLOG_TeamMembers>();
+
+
+            using (DataTable dt = db.Select("SELECT [Id],[Name],[Job],[Tozihat],[github],[Linkedin],[Instagram],[ImagePath],[ImageValue]FROM [PandaMarketCMS].[dbo].[tbl_BLOG_TeamMembers]"))
+            {
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+
+                    tbl_BLOG_TeamMembers tbt = new tbl_BLOG_TeamMembers();
+
+                    tbt.Id = dt.Rows[i]["Id"].ToString();
+                    tbt.Name = dt.Rows[i]["Name"].ToString();
+                    tbt.Job = dt.Rows[i]["Job"].ToString();
+                    tbt.ImagePath = dt.Rows[i]["ImagePath"].ToString();
+                    tbt.ImageValue = dt.Rows[i]["ImageValue"].ToString();
+                    TeamMembers.Add(tbt);
+
+                }
+
+            }
+
+
+            return TeamMembers;
+        }
+
+
+
+    }
+
+}
